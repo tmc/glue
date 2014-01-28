20dfa7851487c0aebbe3ff95f1f8585fc01ae096
Travis Cline <travis.cline@gmail.com>
2014-01-28 09:14:06 -0800

    Make apache logger actually make NSCA-style output

--- loggers/apache.go
+++ loggers/apache.go
@@ -3,6 +3,7 @@ package loggers
 import (
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/tmc/glue"
@@ -12,10 +13,38 @@ import (
 // message to the registered log.Logger within the glue.Context
 // panics if no log.Logger is registered
 func NewApacheLogger() glue.Handler {
-	return func(w http.ResponseWriter, r *http.Request, logger *log.Logger) glue.AfterHandler {
+	return func(w *glue.ResponseWriter, r *http.Request, logger *log.Logger) glue.AfterHandler {
+
+		remoteAddr := r.RemoteAddr
+		if index := strings.LastIndex(remoteAddr, ":"); index != -1 {
+			remoteAddr = remoteAddr[:index]
+		}
+		referer := r.Referer()
+		if "" == referer {
+			referer = "-"
+		}
+		userAgent := r.UserAgent()
+		if "" == userAgent {
+			userAgent = "-"
+		}
 		start := time.Now()
 		return func(c glue.Context) {
-			log.Println(r.Method, r.RequestURI, "took", time.Now().Sub(start))
+
+			logger.Printf(
+				"%s %s %s [%v] \"%s %s %s\" %d %d \"%s\" \"%s\" %d\n",
+				remoteAddr,
+				"-", // remote logname, not supported
+				"-", // @todo get username from request
+				start.Format("02/Jan/2006:15:04:05 -0700"),
+				r.Method,
+				r.RequestURI,
+				r.Proto,
+				w.Status,
+				w.Size,
+				referer,
+				userAgent,
+				time.Now().Sub(start).Nanoseconds()/10e3,
+			)
 		}
 	}
 }
